9bdc989fddd787a321536d3d410f5967bba44e46
Bweka <vincentbweka3@gmail.com>
2022-11-16 08:09:43 +0100

    condition

---
+++ main.py
@@ -0,0 +1,11 @@
+if __name__=="__main__" : 
+
+    a=34
+    b=45
+    c=70
+    if b < a :
+        print("je suis malade")
+    elif c >= b :
+        print("tu vas guerir ne t'en fait pas")
+    else :
+        print("je ne suis pas malade")
